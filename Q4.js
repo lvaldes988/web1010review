8ec344c696b3032476ce897479db53e37af232ad
liset valdes <lvaldes@techlaunch.io>
2017-12-21 17:29:15 -0500

    first commit

---
+++ Q4.js
@@ -0,0 +1,11 @@
+// Question 4
+
+// In a console.log, build the following string in one line of code
+// using special characters for the newline (\n), tab stop (\t), and
+// quotation marks (\"):
+//
+//      "It ain't over til
+//      it's over."
+//             - Yogi Berra
+
+console.log('\t \"It aint over til  \n  \t its over'. \n, \t,  'yogi berra');
